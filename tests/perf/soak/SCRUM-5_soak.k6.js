277c01b2b5cb88bb104e50e7e2ee78e97c408d7b
Nithyananda P <nithyananda.p@dbizsolution.com>
2026-04-21 22:32:56 +0530

    chore(qa-pipeline): auto-run SCRUM-5 -- 15/17 passed -- 2026-04-21 17:02:55

--- tests/perf/soak/SCRUM-5_soak.k6.js
+++ tests/perf/soak/SCRUM-5_soak.k6.js
@@ -1,6 +1,6 @@
 // storyKey: SCRUM-5
 // testType: soak
-// generated: 2026-04-21T16:44:09.176Z
+// generated: 2026-04-21T16:55:03.373Z
 // WARNING: Journey could not be inferred — manual customisation required.
 
 import http from 'k6/http';
--- tests/perf/stress/SCRUM-5_stress.k6.js
+++ tests/perf/stress/SCRUM-5_stress.k6.js
@@ -1,6 +1,6 @@
 // storyKey: SCRUM-5
 // testType: stress
-// generated: 2026-04-21T16:44:09.170Z
+// generated: 2026-04-21T16:55:03.368Z
 // WARNING: Journey could not be inferred — manual customisation required.
 
 import http from 'k6/http';
